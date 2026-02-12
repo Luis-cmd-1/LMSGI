00ef4c8f4ca0d24f491f241135ed76d5ef2b1336
Luis-cmd-1 <langjay496@g.educaand.es>
2026-02-12 14:28:28 +0100

    12/02/2026

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/header'
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-red-500 text-white flex items-end justify-end">
+      <h1 className="text-5xl font-bold">Mi Portfolio con React-TS + Tailwind v4 🚀</h1>
+    </div>
+    <header1/>
+    // <Header/>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header (){
+    return(
+        <header>
+            <nav>
+                <a href="#hero">Incio</a>
+                <a href="#about">Sobre mí</a>
+                <a href="#skills">Skills</a>
+            </nav>
+        </header>
+    );
+}
