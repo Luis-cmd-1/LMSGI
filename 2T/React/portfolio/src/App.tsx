0e0ca31be1e55f79fe2321e08c6b51ad4ad6ed66
Luis-cmd-1 <langjay496@g.educaand.es>
2026-02-16 13:27:25 +0100

    16/02/2026

---
+++ 2T/React/portfolio/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+// import Header from './components/Header';
+import Header1 from './components/Header1';
+// import { MainLayout } from './layout/MainLayout';
+
+
+function App() {
+
+  return (
+    
+       <Header1/>
+  );
+}
+
+export default App
